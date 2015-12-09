0d5e25c759141c88bef62fcd0b1ffe5be327cc22
Yale Thomas <lordchair@gmail.com>
2015-12-08 19:01:45 -0800

    almost hello world

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
+var App = React.createClass({
+  render() {
+    return (
+      <div> HALLO WORLD </div>
+    );
+  }
+});
 
+React.render(new HelloWorld(), document.)
 
 ReactDOM.render(
-  <div>EHLLO OWRLD</div>
+  <App />
 );
+
